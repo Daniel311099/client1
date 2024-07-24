87cd16be73309d627414171f16c6a673f93deb41
Daniel311099 <danielfisaha@gmail.com>
2024-07-24 09:49:33 +0200

    type check

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -27,6 +27,7 @@ const config = {
     '@typescript-eslint/consistent-type-imports': 'off',
     '@typescript-eslint/no-unsafe-argument': 'off',
     '@next/next/no-img-element': 'off',
+    '@typescript-eslint/non-nullable-type-assertion-style': 'off',
  
     // "@typescript-eslint/consistent-type-imports": [
     //   "warn",
